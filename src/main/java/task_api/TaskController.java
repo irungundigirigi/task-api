750888074be6b4830351d9e6183c04cd819ff1d5
edwin irungu <irungundigirigi@gmail.com>
2024-07-10 13:05:34 +0300

    obj not found error handler

--- src/main/java/task_api/TaskController.java
+++ src/main/java/task_api/TaskController.java
@@ -32,8 +32,8 @@ class TaskController {
 
     @GetMapping("/tasks/{id}")
     Task one(@PathVariable Long id) {
-        return repository.findById(id);
-            //.orElseThrow(() -> new TaskNotFoundException(id));
+        return repository.findById(id)
+            .orElseThrow(() -> new TaskNotFoundException(id));
     }
 }
 
