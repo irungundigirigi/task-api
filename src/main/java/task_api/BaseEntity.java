97111ef72799adda48796993fe40aec3a82ec842
edwin irungu <irungundigirigi@gmail.com>
2024-07-11 11:25:02 +0300

    clean up code

--- src/main/java/task_api/BaseEntity.java
+++ src/main/java/task_api/BaseEntity.java
@@ -1,8 +1,5 @@
 package task_api;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Date;
 import java.util.Objects;
 
@@ -11,6 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -29,19 +29,5 @@ public abstract class BaseEntity {
 
     @UpdateTimestamp
     private Date updated_at;
-
-    /** 
-    public Long getId() {
-        return id;
-    }
-
-    public Date getCreated_at() {
-        return created_at;
-    }
-
-    public Date getUpdated_at() {
-        return updated_at;
-    }
-    */
 
 }
--- src/main/java/task_api/Task.java
+++ src/main/java/task_api/Task.java
@@ -3,29 +3,21 @@ package task_api;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.persistence.Entity;
-
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Getter
 @Setter
 public class Task extends BaseEntity {
 
-    
     private String object;
-    
     private String task_priority;
-    
     private Integer status_id;
-    
     private String subject;
-    
     private String description;
-    
     private String due_date;
 
-    public Task() {
-    }
 }
