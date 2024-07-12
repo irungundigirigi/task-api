48a56518d9b84724b7f8c79c669b162c03c06cf4
edwin irungu <irungundigirigi@gmail.com>
2024-07-12 09:54:15 +0300

    update controller to use TaskService

--- v1/src/main/java/task_api/TaskController.java
+++ v1/src/main/java/task_api/TaskController.java
@@ -1,9 +1,13 @@
 package task_api;
 
 import java.util.List;
+import java.util.Optional;
 
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,28 +16,53 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 @RestController
-class TaskController {
+@RequestMapping("/api/v1")
+public class TaskController {
     
-    private final TaskRepository repository;
+    private final TaskService taskService;
 
-    TaskController(TaskRepository repository) {
-        this.repository = repository;
+    TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
-
-    @GetMapping("/tasks")
-    List<Task> all() {
-        return repository.findAll();
-    }
-
+    /**
+     * This method is called when a POST request is made
+     * URL: {hostname}/api/v1/tasks
+     * Purpose: Adds a task to the tasks table
+     * @return Created Task
+    */
     @PostMapping("/tasks")
-    Task newTask(@RequestBody Task newTask) {
-        return repository.save(newTask);
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task savedTask = taskService.saveTask(task);
+        return ResponseEntity.ok().body(taskService.saveTask(savedTask));
     }
 
+    /**
+     * This method is called when a GET request is made
+     * URL: {hostname}/api/v1/tasks
+     * Purpose: Fetches all the tasks in the tasks table
+     * @return List of Tasks
+    */
+    @GetMapping("/tasks")
+    public ResponseEntity<List<Task>> getAllTasks(){
+        List<Task> tasks = taskService.getAllTasks();
+        return ResponseEntity.ok().body(tasks);
+    }
+
+    /**
+     * This method is called when a GET request is made
+     * URL: {hostname}/api/v1/tasks/{id}
+     * Purpose: Fetches specific task table with id {id}
+     * @return Task
+    */
     @GetMapping("/tasks/{id}")
-    Task one(@PathVariable Long id) {
-        return repository.findById(id)
-            .orElseThrow(() -> new TaskNotFoundException(id));
+    public Task getByID(@PathVariable Long id) {
+        Optional<Task> task = taskService.findById(id);
+        if(task.isPresent()) {
+            return task.get();
+
+        }
+            //.orElseThrow(() -> new TaskNotFoundException(id));
+        return null;
     }
 }
 
