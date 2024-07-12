d73e23f654f0b8e418bf21e8bc10c7f14b329d51
edwin irungu <irungundigirigi@gmail.com>
2024-07-12 09:55:45 +0300

    eror handler -TaskNotFound

--- v1/src/main/java/task_api/TaskNotFoundException.java
+++ v1/src/main/java/task_api/TaskNotFoundException.java
@@ -2,6 +2,7 @@ package task_api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.UUID;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TaskNotFoundException extends RuntimeException {
