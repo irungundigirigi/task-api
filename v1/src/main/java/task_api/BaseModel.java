fba828017cab97ed7bb3ea0753ae1b2e48c2d8c4
edwin irungu <irungundigirigi@gmail.com>
2024-07-14 20:56:04 +0300

    use UUID id

--- v1/src/main/java/task_api/BaseModel.java
+++ v1/src/main/java/task_api/BaseModel.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 import lombok.Data;
+import java.util.UUID;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -20,9 +23,15 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Data
 public abstract class BaseModel {
 
+    /*
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
     @CreationTimestamp
     private Date created_at;
