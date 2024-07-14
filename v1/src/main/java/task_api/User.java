773773a2516e3c01c39c0bd84909e0ecc380a36c
edwin irungu <irungundigirigi@gmail.com>
2024-07-14 21:10:46 +0300

    Define relationship user - task

--- v1/src/main/java/task_api/User.java
+++ v1/src/main/java/task_api/User.java
@@ -1,20 +1,29 @@
 package task_api;
 
 import java.util.List;
-import lombok.Data;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.UUID;
 
-
-
+import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "users")
 public class User extends BaseModel {
-    
+
     private String username;
     private String password;
-    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    //private List<Todo> todos;
+
+    @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL)
+    private List<Task> tasks;
 }
+
+
+
