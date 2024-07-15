1550208396441a87c68049727026750dbe36b1b0
edwin irungu <irungundigirigi@gmail.com>
2024-07-15 12:57:46 +0300

    clean up

--- v1/src/main/java/task_api/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package task_api;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-    
-}
