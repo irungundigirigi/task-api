59d3c689101f5017645cd7c130c4af00d94b3322
edwin irungu <irungundigirigi@gmail.com>
2024-07-17 12:11:32 +0300

    handle exception - task not found

---
+++ v1/src/main/java/task_api/exception/TaskNotFoundAdvice.java
@@ -0,0 +1,18 @@
+package task_api.exceptions;
+
+import task_api.exception.TaskNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class TaskNotFoundAdvice {
+
+  @ExceptionHandler(TaskNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  String TaskNotFoundHandler(TaskNotFoundException ex) {
+    return ex.getMessage();
+  }
+}
