5e135622b2869c705ec444b4ce19a8d8a55379d9
edwin irungu <irungundigirigi@gmail.com>
2024-07-26 15:05:31 +0300

    provide ObjectMapper to subclasses

--- v1/src/test/java/task_api/MockMVCBaseClass.java
+++ v1/src/test/java/task_api/MockMVCBaseClass.java
@@ -1,18 +1,21 @@
 package task_api;
 
+import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.context.web.WebAppConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @SpringBootTest
 public abstract class MockMVCBaseClass{
     
     public MockMvc mockMvc;
- 
-    @BeforeEach
+    public final ObjectMapper objMapper = new ObjectMapper();
+  
+    @BeforeEach 
     public void setup(WebApplicationContext wac)  throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
