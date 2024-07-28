904b22cb3078c0e2ec84872187a286db366c53db
edwin irungu <irungundigirigi@gmail.com>
2024-07-28 13:40:14 +0300

    add sql scripts execution phase

--- v1/src/test/java/task_api/MockMVCBaseClass.java
+++ v1/src/test/java/task_api/MockMVCBaseClass.java
@@ -7,8 +7,11 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.context.web.WebAppConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-
+@Sql(scripts = "/cleanup.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = {"/schema.sql", "/data.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
 @SpringBootTest
 public abstract class MockMVCBaseClass{
     
