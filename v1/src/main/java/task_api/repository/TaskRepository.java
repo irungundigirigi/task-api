1d1f3da1e151bcc79d4159933c00e4981c22e696
edwin irungu <irungundigirigi@gmail.com>
2024-07-28 13:34:03 +0300

    clean up code

--- v1/src/main/java/task_api/repository/TaskRepository.java
+++ v1/src/main/java/task_api/repository/TaskRepository.java
@@ -1,15 +1,7 @@
 package task_api.repository;
-
 import task_api.models.Task;
-
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * This interface provides access to data via inbult methods.
- * eg findAll() findById()
- */
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
