edb70dedf8018eb59450b105282337c0be02f604
irungundigirigi <irungundigirigi@gmail.com>
2024-10-11 10:09:44 +0300

    add property object

--- v1/src/main/java/task_api/models/User.java
+++ v1/src/main/java/task_api/models/User.java
@@ -2,7 +2,7 @@ package task_api.models;
 
 import java.util.List;
 import lombok.EqualsAndHashCode;
-
+import jakarta.validation.constraints.NotNull;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,9 +20,20 @@ import lombok.Data;
 @EqualsAndHashCode(callSuper=false)
 public class User extends BaseModel {
 
+    private String object;
+
+    @NotNull
     private String username;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String email;
+
+    public User() {
+        this.object = this.getClass().getSimpleName(); 
+    }
     
     @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL)
     private List<Task> tasks;
