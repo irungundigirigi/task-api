724338053117a58d43ca352e5b5419ddf190d942
irungundigirigi <irungundigirigi@gmail.com>
2024-10-22 14:36:30 +0300

    change type due date from string to Date

--- v1/src/main/java/task_api/models/Task.java
+++ v1/src/main/java/task_api/models/Task.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.Date;
 
 import java.util.UUID;
 
